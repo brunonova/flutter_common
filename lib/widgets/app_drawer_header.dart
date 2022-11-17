c51a59bea5bd0de8df06f8129f5845d61b08b157
Bruno Nova <4999472+brunonova@users.noreply.github.com>
2022-11-17 19:46:26 +0000

    AppDrawerHeader: allow changing the height and disable the background

--- lib/widgets/app_drawer_header.dart
+++ lib/widgets/app_drawer_header.dart
@@ -5,31 +5,36 @@ import '../extensions/build_context.dart';
 /// A [DrawerHeader] that shows the application name over a gradient as
 /// background with colors from the current color scheme.
 class AppDrawerHeader extends StatelessWidget {
-  const AppDrawerHeader({super.key, required this.appName});
-
-  final String appName;
+  const AppDrawerHeader({
+    super.key,
+    required this.title,
+    this.height,
+    this.gradientBackground = true,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return DrawerHeader(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.bottomLeft,
-          end: Alignment.topRight,
-          colors: [
-            // Always from darker to lighter color
-            if (context.theme.brightness == Brightness.dark)
-              context.colorScheme.primaryContainer,
-            context.colorScheme.primary,
-            if (context.theme.brightness == Brightness.light)
-              context.colorScheme.primaryContainer,
-          ],
-        ),
-      ),
+    Widget header = DrawerHeader(
+      decoration: gradientBackground
+          ? BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                colors: [
+                  // Always from darker to lighter color
+                  if (context.theme.brightness == Brightness.dark)
+                    context.colorScheme.primaryContainer,
+                  context.colorScheme.primary,
+                  if (context.theme.brightness == Brightness.light)
+                    context.colorScheme.primaryContainer,
+                ],
+              ),
+            )
+          : null,
       child: Align(
         alignment: Alignment.bottomLeft,
         child: Text(
-          appName,
+          title,
           style: const TextStyle(
             color: Colors.white,
             fontSize: 20,
@@ -37,5 +42,24 @@ class AppDrawerHeader extends StatelessWidget {
         ),
       ),
     );
+
+    if (height != null) {
+      header = SizedBox(
+        height: height,
+        child: header,
+      );
+    }
+
+    return header;
   }
+
+  /// Title of the header.
+  final String title;
+
+  /// Optional height of the header.
+  final double? height;
+
+  /// Whether to show a gradient with the primary colors of the color scheme
+  /// as background.
+  final bool gradientBackground;
 }
